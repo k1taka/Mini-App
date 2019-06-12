3a8fafe193f0f4099679eb0a1092695fb516b8e2
k1taka <sofmori@gmail.com>
2019-06-12 19:38:00 +0900

    create posts
    indexはビューが反映をされているかの確認です。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  resources :posts, only:[:index]
+  resources :users, only:[:index]
+
 end
