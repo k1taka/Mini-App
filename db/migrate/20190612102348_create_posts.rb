2c7ba59cd4af8ac41930ed8b522088b2604d3b51
k1taka <sofmori@gmail.com>
2019-06-12 19:49:48 +0900

    create posts table

--- db/migrate/20190612102348_create_posts.rb
+++ db/migrate/20190612102348_create_posts.rb
@@ -3,6 +3,11 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
 
       t.timestamps
+      t.string      :name,null: false
+      t.text        :text,null: false
+      t.text        :image,null: false
+      t.timestamps null: true
+
     end
   end
 end
