86e202e8c2846650f18566b2517c3f2e914e1323
k1taka <sofmori@gmail.com>
2019-06-13 12:46:06 +0900

    create posts#create&new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
   root 'posts#index'
-  resources :posts, only:[:index]
+  resources :posts, only:[:index,:new,:create]
   resources :users, only:[:index]
 
 end
