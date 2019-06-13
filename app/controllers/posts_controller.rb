4f7f729a352a8acaf4f30b17bfd1f9d5b54a9355
k1taka <sofmori@gmail.com>
2019-06-13 19:50:57 +0900

    create new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,13 +12,11 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
-    redirect_to root_path
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy if post.user_id == current_user.id
-    redirect_to root_path
   end
 
   def edit
