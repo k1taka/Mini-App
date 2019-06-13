452f78b3b81fe71aa5c2fc29b23682c24fcb0238
k1taka <sofmori@gmail.com>
2019-06-13 15:26:04 +0900

    make asosieation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts, only:[:index,:new,:create]
+  resources :posts, only:[:index,:new,:create,:destroy,:edit]
   resources :users, only:[:index]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_042127) do
+ActiveRecord::Schema.define(version: 2019_06_13_060324) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_042127) do
     t.string "name", null: false
     t.text "text", null: false
     t.text "image", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
